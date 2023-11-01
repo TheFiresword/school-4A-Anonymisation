fde1f7eed0741a0b61dcc70a042c05796b7de1f3
Phey69 <mael.portefaix@insa-cvl.fr>
2023-11-01 10:42:01 +0100

    Update des fichiers, toutes attaques faciles faites

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
-import pandas as pd
-from autofill import *
+from function import *
 
+
+# FICHIER 92
 #chargerDATAorigin("0riginal", "dforigin")
-#chargerDATAvictime("S_user_34_c046155c388437412a8755d2f980efb443817bc322e454409e6eeea89714302c", "dfvictimc0")
-#createJoin("dforigin.csv", "dfvictimc0.csv", "jointurec0")
-#createcouple("jointurec0.csv", "resultc0")
-sortbymouth("resultatbe.csv", "finalbe")
+#chargerDATAvictime("S_user_59_92898742d7129cbbf413724f301c258730b979188310cebca9d06ab58c7f5ae0", "dfvictim92")
+createJoin("dforigin.csv", "dfvictim92.csv", "jointure92")
+createcouple("jointure92.csv", "resultat92")
+maxresponse("resultat92.csv", "final92")
+generatejson("final92.csv", "attack92")
+
+# FICHIER a5
+# chargerDATAvictime("S_user_59_a5f2ff1b47e658792f3919d1fd0c04571f4ebff75bfcc74121b7a1429613d49d", "dfvictima5")
+# createJoin("..\dforigin.csv", "dfvictima5.csv", "jointurea5")
+# createcouple("jointurea5.csv", "resultata5")
+# maxresponse("resultata5.csv", "finala5")
+# generatejson("finala5.csv", "attacka5")
+
+# Fichier a3
+# chargerDATAvictime("S_user_59_a365473ee4db27f1b72c6e2c72e0875c508afba82c1511e3501a5f82e58f564a", "dfvictima3")
+# createJoin("..\dforigin.csv", "dfvictima3.csv", "jointurea3")
+# createcouple("jointurea3.csv", "resultata3")
+# maxresponse("resultata3.csv", "finala3")
+# generatejson("finala3.csv", "attacka3")
+
